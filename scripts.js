edad3bac10e3db75d24753e3d5176b8f0ea974c4
JonathanBY15 <bariscanyaprak@gmail.com>
2024-02-29 23:18:06 -0500

    First Commit
    Functioning FAQ component for websites.

---
+++ scripts.js
@@ -0,0 +1,70 @@
+// let questionContainer1 = document.querySelector('#questionContainer1');
+// let questionContainer2 = document.querySelector('#questionContainer2');
+// let questionContainer3 = document.querySelector('#questionContainer3');
+// let questionContainer4 = document.querySelector('#questionContainer4');
+
+// questionContainer1.addEventListener('click', function () {
+//     let answer = document.querySelector('#answer1');
+//     let img = document.querySelector('#questionContainer1 img');
+//     if (answer.style.display !== 'block') {
+//         answer.style.display = 'block';
+//         img.src = 'assets/images/icon-minus.svg'
+//     } else {
+//         answer.style.display = 'none';
+//         img.src = 'assets/images/icon-plus.svg'
+//     }
+// });
+
+// questionContainer2.addEventListener('click', function () {
+//     let answer = document.querySelector('#answer2');
+//     let img = document.querySelector('#questionContainer2 img');
+//     if (answer.style.display !== 'block') {
+//         answer.style.display = 'block';
+//         img.src = 'assets/images/icon-minus.svg'
+//     } else {
+//         answer.style.display = 'none';
+//         img.src = 'assets/images/icon-plus.svg'
+//     }
+// });
+
+// questionContainer3.addEventListener('click', function () {
+//     let answer = document.querySelector('#answer3');
+//     let img = document.querySelector('#questionContainer3 img');
+//     if (answer.style.display !== 'block') {
+//         answer.style.display = 'block';
+//         img.src = 'assets/images/icon-minus.svg'
+//     } else {
+//         answer.style.display = 'none';
+//         img.src = 'assets/images/icon-plus.svg'
+//     }
+// });
+
+// questionContainer4.addEventListener('click', function () {
+//     let answer = document.querySelector('#answer4');
+//     let img = document.querySelector('#questionContainer4 img');
+//     if (answer.style.display !== 'block') {
+//         answer.style.display = 'block';
+//         img.src = 'assets/images/icon-minus.svg'
+//     } else {
+//         answer.style.display = 'none';
+//         img.src = 'assets/images/icon-plus.svg'
+//     }
+// });
+
+
+const questionContainers = document.querySelectorAll('.questionContainer');
+
+questionContainers.forEach(function (container, index) {
+    container.addEventListener('click', function () {
+        const answer = document.querySelector('#answer' + (index + 1));
+        const img = container.querySelector('img');
+
+        if (answer.style.display !== 'block') {
+            answer.style.display = 'block';
+            img.src = 'assets/images/icon-minus.svg';
+        } else {
+            answer.style.display = 'none';
+            img.src = 'assets/images/icon-plus.svg';
+        }
+    });
+});
